a218706cb55011f43f3fdef5c234c7a0e59a5689
EC2 Default User <ec2-user@ip-172-31-1-55.ap-south-1.compute.internal>
2025-05-30 16:06:12 +0000

    Initial commit with Docker setup

---
+++ app.js
@@ -0,0 +1,12 @@
+const http = require('http');
+
+const PORT = process.env.PORT || 80;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.end('Hello from Dockerized Node.js App on EC2!');
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
